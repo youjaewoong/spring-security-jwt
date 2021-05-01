b14bc78f33dfbcd3cde25a77c2b7477bee19f2e2
gos1004 <nnv711@gmail.com>
2021-05-02 02:06:50 +0900

    스프링 security 제외

---
+++ jwt-tutorial/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		//해당 요청은 security 제외
+		http
+			.authorizeRequests()
+			.antMatchers("/api/hello").permitAll()
+			.anyRequest().authenticated();
+	}
+}
