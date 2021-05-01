4d50888f13ff4b89b5d1466a95a4d4b7966f9b71
gos1004 <nnv711@gmail.com>
2021-05-02 02:32:23 +0900

    시큐리트 설정 http, web 접근 설정 DATA 설정

--- jwt-tutorial/src/main/java/com/example/config/SecurityConfig.java
+++ jwt-tutorial/src/main/java/com/example/config/SecurityConfig.java
@@ -1,12 +1,23 @@
 package com.example.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                        ,"/error"
+                );
+    }
+    
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		//해당 요청은 security 제외
